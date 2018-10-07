814e5bbbd7b9661d7772364ca715eb4154ced40d
Junhyun Bae <v__do_ob__v@naver.com>
2018-10-07 18:02:39 +0900

    final bowling.py

--- test.py
+++ test.py
@@ -8,13 +8,14 @@ radius = 0.1085 # meter
 I = 2 / 5 * mass * radius * radius # inertia of sphere
 g = 9.8 # m/s^2
 u_k = 0.08 # friction coeff. 
+
 t = 0.01 #time slice = 0.01 [sec]
 
 x_position = 0 # fix
 y_position = 0 # fix
 
-x_w = 28.28 # [rad/s]
-y_w = -7
+x_w = 7.76 # [rad/s]
+y_w = 0
 
 x_theta = 0
 y_theta = 0
@@ -25,50 +26,54 @@ y = [0]
 
 # initial velocity, calculated by sengsing values
 init_x = 8  # average...
-init_y = 0.1 # ?
-
-
-# initial acc. result of sensoing values
-#x_acc = 6 
-#y_acc = 0.1
+init_y = 0 # ?
 
 x_v = []
 y_v = []
 
 for i in range(400): # count 4 sec...
 
+    count = i
 
     x_v.append(x_w)
     y_v.append(y_w)
 
-    #x_position = x_position + 0.5*x_acc*t*t + init_x*t # s = vt + 0.5at^2 suppose same acc. in timeslice
-    #y_position = y_position + 0.5*y_acc*t*t + init_y*t
     x_temp = x_position
     y_temp = y_position
 
     x_theta_temp = x_theta
     y_theta_temp = y_theta
 
-    x_position = x_position + init_x*t - 0.5*u_k*g*t*t + radius*x_w*t + (0.5*u_k*mass*g*radius*radius*t*t / I)
-    y_position = y_position + init_y*t - 0.5*u_k*g*t*t + radius*y_w*t + (0.5*u_k*mass*g*radius*radius*t*t / I)
+    x_position = x_position + init_x*t
+    y_position = y_position + init_y*t
 
-    x_theta = x_theta + x_w*t + (0.5*u_k*mass*g*radius*t*t / I)
-    y_theta = y_theta +y_w*t + (0.5*u_k*mass*g*radius*t*t / I)
-
-    x_w = (x_theta - x_theta_temp) / t
-    y_w = (y_theta - y_theta_temp) / t
+    x_v = init_x
+    y_v = init_y
     
     x.append(x_position)
     y.append(y_position)
 
-    init_x = (x_position - x_temp) / t
-    init_y = (y_position - y_temp) / t
+    if (init_x - u_k*g*count*t) < 0:
+        for i in range(400-count):
+            #x_v.append(x_w)
+            #y_v.append(y_w)
 
+            x_v = init_x + u_k*g*t
+            y_v = init_y + u_k*g*t
 
-    #x_acc = x_acc * rand # update acc. calculated by friction, inertia, etc,.
-    #y_acc = y_acc * rand
+            x_temp = x_position
+            y_temp = y_position
 
-    #rand = rand - rand*0.1 # temp for test
+            x_position = x_position + init_x*t + 0.5*u_k*g*t*t
+            y_position = y_position + init_y*t + 0.5*u_k*g*t*t
+            
+            x.append(x_position)
+            y.append(y_position)
+            #if (init_x > radius*)
+
+        break;
+
+    
 
     
 '''
