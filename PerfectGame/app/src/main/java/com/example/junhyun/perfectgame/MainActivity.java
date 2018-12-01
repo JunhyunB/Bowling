f186fc311cff616333fa71c04c11de186c3a914d
Junhyun Bae <v__do_ob__v@naver.com>
2018-12-01 16:01:26 +0900

    update animation effect

--- PerfectGame/app/src/main/java/com/example/junhyun/perfectgame/MainActivity.java
+++ PerfectGame/app/src/main/java/com/example/junhyun/perfectgame/MainActivity.java
@@ -4,7 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,8 +17,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ImageView logo = (ImageView) findViewById(R.id.logo1);
+        final Animation scale = AnimationUtils.loadAnimation(this, R.anim.scale);
+        logo.startAnimation(scale);
+
+        TextView title = (TextView) findViewById(R.id.textView);
+        title.startAnimation(scale);
+
         Button button1 = (Button) findViewById(R.id.swingpractice);
         button1.setOnClickListener(new View.OnClickListener()      {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), touch_main.class);
+                startActivity(intent);
+            }
+        });
+
+        Button game = (Button) findViewById(R.id.minigame);
+        game.setOnClickListener(new View.OnClickListener()      {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), touch_main.class);
