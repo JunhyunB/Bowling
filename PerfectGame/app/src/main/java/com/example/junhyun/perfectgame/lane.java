03fea72879028ee1ca077b5b1913b8770ffda21d
Junhyun Bae <v__do_ob__v@naver.com>
2018-12-15 19:19:49 +0900

    Big update!

--- PerfectGame/app/src/main/java/com/example/junhyun/perfectgame/lane.java
+++ PerfectGame/app/src/main/java/com/example/junhyun/perfectgame/lane.java
@@ -7,47 +7,122 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.ViewTreeObserver;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class lane extends AppCompatActivity {
     ImageView imageView;
+    ImageView pin1;
+    ImageView pin2;
+    ImageView pin3;
+    ImageView pin4;
+    ImageView pin5;
+    ImageView pin6;
+    ImageView pin7;
+    ImageView pin8;
+    ImageView pin9;
+    ImageView pin10;
+    int count = 0;
+    int on1 = 1;
+    int on2 = 1;
+    int on3 = 1;
+    int on4 = 1;
+    int on5 = 1;
+    int on6 = 1;
+    int on7 = 1;
+    int on8 = 1;
+    int on9 = 1;
+    int on10 = 1;
+
+    int [] temp = {0, 0};
+
+
+    TextView t;
+
+
     int q=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lane_main);
-
+        t = findViewById(R.id.textView4);
+        pin1 = findViewById(R.id.imageView);
+        pin2 = findViewById(R.id.imageView2);
+        pin3 = findViewById(R.id.imageView3);
+        pin4 = findViewById(R.id.imageView4);
+        pin5 = findViewById(R.id.imageView5);
+        pin6 = findViewById(R.id.imageView6);
+        pin7 = findViewById(R.id.imageView7);
+        pin8 = findViewById(R.id.imageView8);
+        pin9 = findViewById(R.id.imageView9);
+        pin10 = findViewById(R.id.imageView10);
         Intent intent = getIntent();
         Bundle b1 = intent.getBundleExtra("bundle");
         //Bundle b2 = getIntent().getBundleExtra("y_result");
+        final boolean bb;
 
         final ArrayList y_ = b1.getParcelableArrayList("x_result");
         final ArrayList x_ = b1.getParcelableArrayList("y_result");
-/*
         for(int i=0;i<x_.size();i++){
             Log.d("x값:",String.valueOf(x_.get(i)));
             Log.d("y값:",String.valueOf(y_.get(i)));
         }
-*/
 
         imageView = findViewById(R.id.temp);
         ViewTreeObserver viewTreeObserver = this.imageView.getViewTreeObserver();
+
         this.imageView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             public void onGlobalLayout() {
 
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
-                        for (int i = 0; i < x_.size(); i++) {
+                        for (int i = 0; i < x_.size()-1; i++) {
                             try {
                                 Thread.sleep(20);
                                 runOnUiThread(new Runnable() {
                                     @Override
                                     public void run() {
-                                        imageView.setX(imageView.getX() + (float) x_.get(q)); //좌우로
-                                        imageView.setY(imageView.getY() - (float) y_.get(q)); //위로
+                                        imageView.setX(imageView.getX() + ((float) x_.get(q)*5)); //좌우로
+                                        imageView.setY(imageView.getY() - ((float) y_.get(q)*0.7f)); //위로
+
+                                        temp = collision(imageView, pin1, count, on1);
+                                        count = temp[0];
+                                        on1 = temp[1];
+                                        temp = collision(imageView, pin2, count, on2);
+                                        count = temp[0];
+                                        on2 = temp[1];
+                                        temp = collision(imageView, pin3, count, on3);
+                                        count = temp[0];
+                                        on3 = temp[1];
+                                        temp = collision(imageView, pin4, count, on4);
+                                        count = temp[0];
+                                        on4 = temp[1];
+                                        temp = collision(imageView, pin5, count, on5);
+                                        count = temp[0];
+                                        on5 = temp[1];
+                                        temp = collision(imageView, pin6, count, on6);
+                                        count = temp[0];
+                                        on6 = temp[1];
+                                        temp = collision(imageView, pin7, count, on7);
+                                        count = temp[0];
+                                        on7 = temp[1];
+                                        temp = collision(imageView, pin8, count, on8);
+                                        count = temp[0];
+                                        on8 = temp[1];
+                                        temp = collision(imageView, pin9, count, on9);
+                                        count = temp[0];
+                                        on9 = temp[1];
+                                        temp = collision(imageView, pin10, count, on10);
+                                        count = temp[0];
+                                        on10 = temp[1];
+                                            //t.setText("성공");
+
+
+                                        t.setText("Score : " + Integer.toString(count));
+
                                     }
                                 });
                                 q++;
@@ -56,6 +131,7 @@ public class lane extends AppCompatActivity {
                         }
                     }
                 }).start();
+
                 ViewTreeObserver obs = lane.this.imageView.getViewTreeObserver();
                 if (Build.VERSION.SDK_INT >= 16) {
                     obs.removeOnGlobalLayoutListener(this);
@@ -65,5 +141,28 @@ public class lane extends AppCompatActivity {
 
             }
         });
+    }
+    public int[] collision(ImageView a, ImageView b, int count, int on){
+        float bl = a.getY();
+        float bt = a.getX();
+        float br = a.getWidth() + bl;
+        float bb = a.getHeight() + bt;
+        float pl = b.getY();
+        float pt = b.getX();
+        float pr = b.getWidth() + pl;
+        float pb = b.getHeight() + pt;
+
+        if (on == 1) {
+            if (bl <= pr && bl >= pl && bt >= pt && bt <= pb) {
+                return new int[]{count+1, 0};
+            } else if (br >= pl && br <= pr && bb >= pt && bb <= pb) {
+                return new int[]{count+1, 0};
+            } else if (bt <= pb && bt >= pt && br >= pl && br <= pr) {
+                return new int[]{count+1, 0};
+            } else if (bb >= pt && bb <= pb && bl >= pl && bl <= pr) {
+                return new int[]{count+1, 0};
+            }
+        }
+        return new int[] {count, on};
     }
 }
--- PerfectGame/app/src/main/java/com/example/junhyun/perfectgame/touch_main.java
+++ PerfectGame/app/src/main/java/com/example/junhyun/perfectgame/touch_main.java
@@ -3,6 +3,10 @@ package com.example.junhyun.perfectgame;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.SoundPool;
@@ -10,6 +14,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -17,6 +22,7 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,9 +33,41 @@ public class touch_main extends AppCompatActivity {
     ArrayList x_result = new ArrayList<Float>();
     ArrayList y_result = new ArrayList<Float>();
 
+    ArrayList x_acc = new ArrayList<Float>();
+    ArrayList y_acc = new ArrayList<Float>();
+    ArrayList z_acc = new ArrayList<Float>();
+
+    ArrayList x_gyro = new ArrayList<Float>();
+    ArrayList y_gyro = new ArrayList<Float>();
+    ArrayList z_gyro = new ArrayList<Float>();
+
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private Sensor mMagneticField;
+    private Sensor mGgyroSensor = null;
+    private SensorEventListener mAccelLis;
+    private SensorEventListener mMagLis;
+    private SensorEventListener mGyroLis;
+
+    private double RAD2DGR = 180 / Math.PI;
+    private static final float NS2S = 1.0f/1000000000.0f;
+    private float mAzimut;
+    //private float mPitch;
+    //private float mRoll;
+    double gyroX;
+    double gyroY;
+    double gyroZ;
+    ArrayList mPitch = new ArrayList<Float>(); //각속도만 어레이로 받았음
+    ArrayList mRoll = new ArrayList<Float>();
+    private TextView mResultView;
+
+    float[] mGravity;
+    float[] mGeomagnetic;
+
+
     public List physics(float mass, float theta,
-                           float x_vel, float y_vel,
-                           float x_ang, float y_ang) {
+                        float x_vel, float y_vel,
+                        float x_ang, float y_ang) {
 
         float radius = 0.1085F; // [kg]
         float g = 9.8F; // [m/s^2]
@@ -161,7 +199,7 @@ public class touch_main extends AppCompatActivity {
         setContentView(R.layout.activity_touch_main);
 
         Intent intent = getIntent();
-        float mass = intent.getExtras().getFloat("mass");
+        final float mass = intent.getExtras().getFloat("mass");
         int gamecheck = intent.getExtras().getInt("gamecheck"); // swing practice 와 mini game 을 구분해야함.
 
         ImageView B1 = (ImageView) findViewById(R.id.touchB1);
@@ -171,33 +209,138 @@ public class touch_main extends AppCompatActivity {
         B1.startAnimation(alpha);
         B2.startAnimation(alpha);
 
+        mResultView = (TextView) findViewById(R.id.textView2);
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mAccelLis = new SenserListener();
+        mMagneticField = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        mMagLis = new SenserListener();
+
         soundpool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
         sound = soundpool.load(this, R.raw.sound1, 1);
         soundpool.play(sound, 1, 1, 0, 1, 1);
 
         ImageButton button2 = (ImageButton) findViewById(R.id.touchB1);
-        button2.setOnClickListener(new View.OnClickListener()      {
+
+
+        button2.setOnTouchListener(new View.OnTouchListener()      {
+            //int i=0; //어레이실패
+
+            SenserListener SL = new SenserListener();
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+
+                    case MotionEvent.ACTION_DOWN:
+                        break;
+
+                    case MotionEvent.ACTION_MOVE:
+                        //SL.onSensorChanged(); // 이건 뭐지?
+                        mSensorManager.registerListener(mAccelLis ,mAccelerometer, SensorManager.SENSOR_DELAY_UI);
+                        mSensorManager.registerListener(mMagLis, mMagneticField, SensorManager.SENSOR_DELAY_UI);
+                        mSensorManager.registerListener(mGyroLis, mGgyroSensor, SensorManager.SENSOR_DELAY_UI);
+                        String result;
+                        result = "Azimut:"+mAzimut+"\n"+"Pitch:"+mPitch+"\n"+"Roll:"+mRoll+"\n"+"gyroX:"+gyroX+"\n"+"gyroY:"+gyroY+"\n"+"gyroZ:"+gyroZ+"\n";
+                        //result = "Azimut:"+mAzimut+"\n"+"Pitch:"+mPitch.get(i)+"\n"+"Roll:"+mRoll.get(i); //이 부분이 어레이로 받아서 시도했는데 안되는 부분이에요 ㅠ
+                        mResultView.setText(result);
+
+                        //i++; //어레이 실패
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        List L = physics(mass, 8, 6, 0, 7, 12);
+                        x_result = (ArrayList<Float>)L.get(0);
+                        y_result = (ArrayList<Float>)L.get(1);
+                        Intent intent = new Intent(getApplicationContext(), lane.class);
+                        Bundle b = new Bundle();
+                        b.putParcelableArrayList("x_result", x_result);
+                        b.putParcelableArrayList("y_result", y_result);
+                        intent.putExtra("bundle", b);
+                        startActivity(intent);
+                        break;
+
+                    default:
+                        break;
+                }
+
+                return false;
+            }
+
+        });
+    }
+    /* //지워야하는가.....
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mMagneticField, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+*/
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(mAccelLis);
+    }
+
+    private class SenserListener implements SensorEventListener {
         @Override
-        public void onClick(View v) {
+        public void onSensorChanged(SensorEvent event) {
 
-            //Background thread = new Background();
-            //thread.start();
 
-            List L = physics(7, 8, 6, 0, 7, 12);
-            x_result = (ArrayList<Float>)L.get(0);
-            y_result = (ArrayList<Float>)L.get(1);
+            if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
 
-            Intent intent = new Intent(getApplicationContext(), lane.class);
+                /*
+                gyroX = event.values[0];
+                gyroY = event.values[1];
+                gyroZ = event.values[2];
 
-            Bundle b = new Bundle();
-            b.putParcelableArrayList("x_result", x_result);
-            b.putParcelableArrayList("y_result", y_result);
-            intent.putExtra("bundle", b);
 
-            startActivity(intent);
+                dt = (event.timestamp - timestamp) * NS2S;
+                timestamp = event.timestamp;
+
+                if (dt - timestamp*NS2S != 0) {
+                    // 각속도 성분을 적분 -> 회전각(pitch, roll)으로 변환.
+                    pitch = pitch + gyroY*dt;
+                    roll = roll + gyroX*dt;
+                    yaw = yaw + gyroZ*dt;
+                }
+                */
+
+                x_gyro.add(event.values[0]); //자이로 XYZ 받아오는 변수들입니다
+                y_gyro.add(event.values[1]);
+                z_gyro.add(event.values[2]);
+            }
+
+            if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {//가속도 센서 감지
+                //mGravity = event.values;
+                x_acc.add(event.values[0]);
+                y_acc.add(event.values[1]);
+                z_acc.add(event.values[2]);
+
+            }
+
+            if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) //지자기 센서 감지
+                mGeomagnetic = event.values;
+
+            if (mGravity != null && mGeomagnetic != null) { //가속도와 지자기센서
+                float R[] = new float[9];
+                float I[] = new float[9];
+                boolean success = SensorManager.getRotationMatrix(R, I, mGravity, mGeomagnetic);
+                if (success) {
+                    float orientation[] = new float[3];
+                    SensorManager.getOrientation(R, orientation);
+                    mAzimut = (float) Math.toDegrees(orientation[0]); //방위각 추가
+                    mPitch.add((float) Math.toDegrees(orientation[1])); //pitch List에 추가
+                    mRoll.add((float) Math.toDegrees(orientation[2])); //roll List에 추가
+                    //mPitch = (float) Math.toDegrees(orientation[1]);
+                    //mRoll = (float) Math.toDegrees(orientation[2]);
+                }
+            }
         }
-    });
-}
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    }
 
     class Background extends Thread {
         @Override
